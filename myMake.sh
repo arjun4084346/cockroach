a1d674716ad80824b45f33a207ba647208638258
Bora <arjun4084346@gmail.com>
2016-07-21 11:34:50 -0700

    changed makefile to start 3 node cluster

--- myMake.sh
+++ myMake.sh
@@ -1,6 +1,10 @@
 #./cockroach quit
 killall -9 cockroach
-rm -rf cockroach
+#rm -rf cockroach
+rm -rf ./node1
+rm -rf ./node2
+rm -rf ./node3
 make build
-./cockroach start --background --host=localhost #--port=26258 --pgport=15433
-
+./cockroach start --background --host=localhost --store=path=./node1
+./cockroach start --background --host=localhost --store=path=./node2 --join=localhost:26257 --http-port=8082 --port=26258
+./cockroach start --background --host=localhost --store=path=./node3 --join=localhost:26257 --http-port=8083 --port=26259
